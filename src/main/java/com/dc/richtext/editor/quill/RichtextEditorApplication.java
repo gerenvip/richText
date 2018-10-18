36128a843664c048c3bbc7043929502f49c7727c
wangwei <wangwei@jiandaola.com>
2018-10-18 11:35:35 +0800

    init commit

---
+++ src/main/java/com/dc/richtext/editor/quill/RichtextEditorApplication.java
@@ -0,0 +1,12 @@
+package com.dc.richtext.editor.quill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichtextEditorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RichtextEditorApplication.class, args);
+    }
+}
